fcb0896bf7fbd49032fa741167f5dadcf2cab0d1
Nanditabharti140 <nanditabharti628@gmail.com>
2026-02-07 01:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (17.15%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -3,40 +3,46 @@ class MyQueue {
     Stack<Integer> helper = new Stack<>();
 
     public MyQueue() {
-        
     }
-    
-    public void push(int x) {
+
+    public void push(int x) {  
+        //push at bottom of stack
+        while(st.size() > 0) helper.push(st.pop());
         st.push(x);
+        while(helper.size() > 0) st.push(helper.pop());
     }
     
     public int pop() {
-        //st ka bottom remove kro
-        while(st.size() > 1) helper.push(st.pop());
-        int front = st.pop();
-        while(helper.size() > 0) st.push(helper.pop());
-        return front;
+       return st.pop();
     }
     
     public int peek() {
-         //st ka bottom print kro
-        while(st.size() > 1) helper.push(st.pop());
-        int front = st.peek();
-        while(helper.size() > 0) st.push(helper.pop());
-        return front;
+     return st.peek();
     }
     
+    // public void push(int x) {    0(1) this is push efficient approach
+    //     st.push(x);
+    // }
+    
+    // public int pop() {  o(n)
+    //     //st ka bottom remove kro
+    //     while(st.size() > 1) helper.push(st.pop());
+    //     int front = st.pop();
+    //     while(helper.size() > 0) st.push(helper.pop());
+    //     return front;
+    // }
+    
+    // public int peek() {
+    //      //st ka bottom print kro
+    //     while(st.size() > 1) helper.push(st.pop());
+    //     int front = st.peek();
+    //     while(helper.size() > 0) st.push(helper.pop());
+    //     return front;
+    // }
     
     public boolean empty() {
         return (st.size() == 0);
     }
 }
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
+
