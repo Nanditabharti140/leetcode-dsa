f8bfdfe5cb7adc0b7f703d055ca6e655b8f972e5
Nanditabharti140 <nanditabharti628@gmail.com>
2026-03-24 22:22:20 +0530

    Time: 1 ms (99.90%) | Memory: 46.2 MB (94.85%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(char[][] board , int row , int col, char num){
+        //check the entire col
+        for(int i = 0 ; i < 9 ; i++){
+            if(board[i][col] == num) return false;
+        }
+
+        //check the entire row
+        for(int j = 0 ; j < 9 ; j++){
+            if(board[row][j] == num) return false;
+        }
+
+        //check the entire grid of 3x3 matrix
+        int sr = row/3*3;
+        int sc = col/3*3;
+        for(int i = sr; i < sr+3; i++){
+            for(int j = sc; j < sc+3; j++){
+                if(board[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i < 9 ; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.') continue;
+                char num = board[i][j];
+                board[i][j] = '.';
+                if(isValid(board , i , j , num) == false) return false;
+                board[i][j] = num;
+            }
+        }
+        return true;
+    }
+}
