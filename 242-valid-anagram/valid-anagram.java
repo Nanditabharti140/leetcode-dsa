152c84936f5a62e2a00895dec408f2d39774e7e0
Nanditabharti140 <nanditabharti628@gmail.com>
2026-03-28 16:33:50 +0530

    Time: 33 ms (5.49%) | Memory: 46.6 MB (20.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -13,8 +13,22 @@ class Solution {
     }
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        HashMap<Character, Integer> mp1 = makeFreqMap(s);
-        HashMap<Character, Integer> mp2 = makeFreqMap(t);
-        return mp1.equals(mp2);
+        HashMap<Character, Integer> map = makeFreqMap(s);
+        //HashMap<Character, Integer> mp2 = makeFreqMap(t);
+        //return mp1.equals(mp2);
+
+        // Second method - first make map of String s and then check for t if the key of t is not present then return false
+        // else reduce the value of that key by one and last step is check the map has all value equal to 0
+
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            if(!map.containsKey(ch)) return false;
+            map.put(ch, map.get(ch) - 1);
+        }
+
+        for(Integer i : map.values()){
+            if(i != 0) return false;
+        }
+        return true;
     }
 }
